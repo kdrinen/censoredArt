c95cd4556d8636e637e20e66c0e023511ed08307
Karlie Drinen <kdrinen@150-253-88-27.wifi.mcad.edu>
2016-10-24 17:37:09 -0500

    init socket.io/server interaction

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ var app = express(); //use the express js framework to serve out html files (han
 var server = require('http').Server( app ) //start a server instance on a port
 var io = require('socket.io')(server) //use socket.io for real time connections aka. wesockets
 
-server.listen(3000, function(){ //set up a server on posrt 3000, do a callback when it started successfully
-  console.log("server started on 3000");
+server.listen(3100, function(){ //set up a server on posrt 3000, do a callback when it started successfully
+  console.log("server started on 3100");
 })
 
 app.use( express.static('public'))
 
-var storedPositions = [];
+var storedPositions = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true];
+
 
 io.on('connection', function(socket){ //if socket.io sees a new connection, do something with them...
   console.log(socket.id)//prints out the socket that connected (ie. all users + the projection)
 
-  io.emit('storedPositionMessage', storedPositions);
+    for(i = 0; i <= storedPositions.length;i++){
+      var data = { id:i, status:storedPositions[i] }//pull data by comparing storedPosition to idls -al
+      io.emit('UDmyStatus', data); //send out the stored positions to all users
+    }
 
-  socket.on('addRectangle', function(data){//look for any messages with the 'addRectangle'
-    console.log("addRectangle" + data);//log out the 'data' in this case you get true, but you could use this to get any arbitrary data you want, think position, color, etc.
-    io.emit('projectionRectangle', data);// send out a message to the projection to add a rect to the screen
+  socket.on('getPosition', function(id){ //see if the user updated any positions
+    io.emit('GPmyStatus', { id:id, status:storedPositions[id] }); //send out the stored positions to everyone connected.
+  })
 
-    storedPositions.push(data);
+  socket.on('updateMyStatus',function(data){
+    storedPositions[data.id] =  data.status;
+    // console.log(storedPositions);
+    var newData = { id:data.id, status:storedPositions[data.id] }//when there is an update in positions, update storedPositions
+    io.emit('UDmyStatus', newData); //send out the stored positions to everyone connected. Constantly sending data out.
+
   })
 
 })
